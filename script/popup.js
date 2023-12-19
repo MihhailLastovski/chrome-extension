6a78935359cf20326d9659f952cb0042ae08b48d
Edgar Neverovski <eneverovski@sqad.loc>
2023-12-19 17:52:50 +0200

    Design added, button changed, icon changing in work

--- script/popup.js
+++ script/popup.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const btn = document.querySelector(".btn");
+  const toggleSwitch = document.querySelector(".toggleSwitch");
   const heading = document.querySelector(".heading");
   const searchTextInput = document.getElementById("searchText");
   const highlightBtn = document.getElementById("highlightBtn");
@@ -28,55 +28,43 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     removeHighlight();
     highlight(); 
+
+    const canvas = new OffscreenCanvas(16, 16);
+    const context = canvas.getContext('2d');
+    context.clearRect(0, 0, 16, 16);
+    context.fillStyle = '#00FF00';  // Green
+    context.fillRect(0, 0, 16, 16);
+    const imageData = context.getImageData(0, 0, 16, 16);
+    chrome.action.setIcon({imageData: imageData});
+    
   });
-  
 
-  btn.addEventListener("click", clickHandler);
-  function clickHandler() {
-    active = !active;
-    chrome.storage.local.set({ isActive: active }).then(() => {
-      console.log("Value is set");
-    });
-    btn.classList.add("animating");
-    btn.addEventListener("animationend", toggleAnimation);
-  }
-
-
-  async function checkBtnIsActive() {
+  async function toggleSwitchIsActive() {
     const result = await new Promise((resolve, reject) => {
       chrome.storage.local.get("isActive", (result) => {
         resolve(result);
       });
     });
+
+    active = result.isActive;
+
+    heading.innerText = active ? "Highlight On" : "Highlight Off";
+    searchTextInput.disabled = !active;
+    highlightBtn.disabled = !active;
+
+    toggleSwitch.checked = active;
+    
+  }
+  toggleSwitchIsActive();
   
-    const boolActive = result.isActive;
-    if (boolActive) {
-      turnOn();
-      active = true;
-    } else {
-      turnOff();
-      active = false;
-    }
-  }
-  checkBtnIsActive();
+  toggleSwitch.addEventListener('change', function() {
+    active = !active;
+    chrome.storage.local.set({ isActive: active });
 
+    heading.innerText = active ? "Highlight On" : "Highlight Off";
+    searchTextInput.disabled = !active;
+    highlightBtn.disabled = !active;
 
-  function toggleAnimation() {
-    btn.classList.remove("animating");
-    active ? turnOn() : turnOff();
-  }
-
-  function turnOn() {
-    btn.classList.add("active");
-    heading.classList.add("active");
-    searchTextInput.disabled = false;
-    highlightBtn.disabled = false;
-  }
-
-  function turnOff() {
-    btn.classList.remove("active");
-    heading.classList.remove("active");
-    searchTextInput.disabled = true;
-    highlightBtn.disabled = true;
-  }
+    
+  });
 });
