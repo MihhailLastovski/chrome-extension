7bc23f7d3153ba3fe1aa527020daf2fb5b8d5562
vafle <vafle@ARTURIK>
2023-12-21 14:33:00 +0200

    Proper darkmode added, broke the button though

--- script/styleScript.js
+++ script/styleScript.js
@@ -1,10 +1,41 @@
-const darkModeToggle = document.getElementById('darkModeToggle');
-    const styleLink = document.getElementById('styleLink');
+//Находит тему из storage
+function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
+  if (localStorageTheme !== null) {
+    return localStorageTheme;
+  }
 
-    darkModeToggle.addEventListener('change', function() {
-      if (this.checked) {
-        styleLink.href = 'style/darkstyle.css'; // Path to your dark mode CSS file
-      } else {
-        styleLink.href = 'style/styles.css'; // Path to your regular CSS file
-      }
+  if (systemSettingDark.matches) {
+    return "dark";
+  }
+
+  return "light";
+}
+//обновляет хтмл тэг
+function updateThemeOnHtmlEl({ theme }) {
+  document.querySelector("html").setAttribute("data-theme", theme);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const darkModeToggle = document.getElementById("darkModeToggle");
+  const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+  //находит примененные настройки
+  chrome.storage.local.get("theme", function (result) {
+    const localStorageTheme = result.theme;
+    let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
+
+    updateThemeOnHtmlEl({ theme: currentThemeSetting });
+
+    //меняет тему
+    darkModeToggle.addEventListener("change", function () {
+      const newTheme = darkModeToggle.checked ? "dark" : "light";
+
+      chrome.storage.local.set({ "theme": newTheme });
+      updateThemeOnHtmlEl({ theme: newTheme });
+
+      currentThemeSetting = newTheme;
     });
+
+    darkModeToggle.checked = currentThemeSetting === "dark";
+  });
+});
