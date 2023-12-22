c249f252fa2e7182f8ce7e8b86aecf10c23dbc89
Artur Link <alink@sqad.loc>
2023-12-22 10:00:44 +0200

    Done n Dusted

--- script/styleScript.js
+++ script/styleScript.js
@@ -7,7 +7,6 @@ function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark })
   if (systemSettingDark.matches) {
     return "dark";
   }
-
   return "light";
 }
 //обновляет хтмл тэг
