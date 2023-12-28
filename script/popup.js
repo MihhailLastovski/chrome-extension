2effe0cbd94cc866685380fab9dc38ab9a4dc27d
Mihhail Lastovski <mlastovski@sqad.loc>
2023-12-28 10:52:45 +0200

    fixed

--- script/popup.js
+++ script/popup.js
@@ -8,17 +8,26 @@ document.addEventListener("DOMContentLoaded", function () {
   const newListBtn = document.getElementById("newListBtn");
   let active;
 
-  const counterElem = document.getElementById('highlightedCount');
+  const counterElem = document.getElementById("highlightedCount");
+  const colorOptions = document.querySelectorAll('.color-option');
+  colorOptions.forEach(option => {
+    if (option.checked) {
+      selectedColor = option.value;
+    }
+  });
+
+  colorOptions.forEach(option => {
+    option.addEventListener('change', function() {
+      selectedColor = this.value; 
+    });
+  });
 
   highlightBtn.addEventListener("click", function () {
     let searchText = searchTextInput.value.trim();
-    selectedColor = document.querySelector('input[name="highlightColor"]:checked').value;
-
-    
 
     function removeHighlight() {
       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { action: "removeHighlight" });       
+        chrome.tabs.sendMessage(tabs[0].id, { action: "removeHighlight" });
       });
     }
 
@@ -36,16 +45,20 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     }
     removeHighlight();
-    highlight(); 
+    highlight();
   });
-  
-  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.action === 'updateBadge') {
+
+  chrome.runtime.onMessage.addListener(function (
+    request,
+    sender,
+    sendResponse
+  ) {
+    if (request.action === "updateBadge") {
       const count = request.count || 0;
-      chrome.action.setBadgeText({ text: count > 0 ? count.toString() : '' });
-      chrome.action.setBadgeBackgroundColor({ color: '#9eff00' });
+      chrome.action.setBadgeText({ text: count > 0 ? count.toString() : "" });
+      chrome.action.setBadgeBackgroundColor({ color: "#9eff00" });
       counterElem.innerHTML = `Word counter: ${count}`;
-      chrome.storage.local.set({count: count});
+      chrome.storage.local.set({ count: count });
     }
   });
 
@@ -55,25 +68,25 @@ document.addEventListener("DOMContentLoaded", function () {
   function renderWordLists(lists) {
     wordLists.innerHTML = "";
     lists.forEach((list) => {
-      const listItem = document.createElement("div");   
+      const listItem = document.createElement("div");
       listItem.className = "wordListsItem";
 
       const textContainer = document.createElement("div");
       textContainer.className = "textContainer";
       textContainer.textContent = list.name;
 
-
       const buttons = document.createElement("div");
-      buttons.className = "buttonsContainer"
+      buttons.className = "buttonsContainer";
       const enableButton = document.createElement("button");
-      enableButton.innerHTML = enabledLists.includes(list.id) ? '<i class="fa fa-pause" aria-hidden="true"></i>' : '<i class="fa fa-play" aria-hidden="true"></i>';
+      enableButton.innerHTML = enabledLists.includes(list.id)
+        ? '<i class="fa fa-pause" aria-hidden="true"></i>'
+        : '<i class="fa fa-play" aria-hidden="true"></i>';
       enableButton.addEventListener("click", function () {
         const enable = !enabledLists.includes(list.id);
         toggleWordList(list.id, enable);
         renderWordLists(lists);
       });
       buttons.appendChild(enableButton);
-
 
       const deleteButton = document.createElement("button");
       deleteButton.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
@@ -83,9 +96,9 @@ document.addEventListener("DOMContentLoaded", function () {
       });
       buttons.appendChild(deleteButton);
 
-
       const updateButton = document.createElement("button");
-      updateButton.innerHTML = '<i class="fa fa-pencil" aria-hidden="true"></i>';
+      updateButton.innerHTML =
+        '<i class="fa fa-pencil" aria-hidden="true"></i>';
       updateButton.addEventListener("click", function () {
         window.location.href = `list.html?listId=${list.id}`;
       });
@@ -115,7 +128,10 @@ document.addEventListener("DOMContentLoaded", function () {
   function toggleWordList(listId, enable) {
     function removeHighlight() {
       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { action: "removeHighlight", listId: listId });
+        chrome.tabs.sendMessage(tabs[0].id, {
+          action: "removeHighlight",
+          listId: listId,
+        });
       });
     }
 
@@ -136,48 +152,52 @@ document.addEventListener("DOMContentLoaded", function () {
     chrome.storage.local.get("wordLists", function (data) {
       const lists = data.wordLists || [];
       const listToHighlight = lists.find((list) => list.id === listId);
-  
+
       if (listToHighlight) {
         const sortedWords = listToHighlight.words.sort((a, b) => {
           return b.word.length - a.word.length;
         });
-  
+
         sortedWords.forEach((wordObj) => {
-          if (wordObj.enabled) { 
+          if (wordObj.enabled) {
             const searchText = wordObj.word.trim();
-            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-              chrome.scripting.executeScript({
-                target: { tabId: tabs[0].id },
-                files: ["./script/contentScript.js"],
-              });
-              chrome.tabs.sendMessage(tabs[0].id, {
-                action: "highlight",
-                searchText: searchText,
-                highlightColor: selectedColor,
-                listId: listId,
-              });
-            });
+            chrome.tabs.query(
+              { active: true, currentWindow: true },
+              function (tabs) {
+                chrome.scripting.executeScript({
+                  target: { tabId: tabs[0].id },
+                  files: ["./script/contentScript.js"],
+                });
+                chrome.tabs.sendMessage(tabs[0].id, {
+                  action: "highlight",
+                  searchText: searchText,
+                  highlightColor: selectedColor,
+                  listId: listId,
+                });
+              }
+            );
           }
         });
       }
     });
   }
-  
-  
 
   function deleteWordList(listId) {
     chrome.storage.local.get("wordLists", function (data) {
       let lists = data.wordLists || [];
       let updatedLists = lists.filter((list) => list.id !== listId);
       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { action: "removeHighlight", listId: listId });
+        chrome.tabs.sendMessage(tabs[0].id, {
+          action: "removeHighlight",
+          listId: listId,
+        });
       });
       chrome.storage.local.set({ wordLists: updatedLists }, function () {
         getAllWordLists(renderWordLists);
       });
     });
   }
-  
+
   async function toggleSwitchIsActive() {
     const result = await new Promise((resolve, reject) => {
       chrome.storage.local.get("isActive", (result) => {
@@ -186,11 +206,11 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     active = result.isActive;
     updateUIState();
-    toggleSwitch.checked = active; 
+    toggleSwitch.checked = active;
   }
   toggleSwitchIsActive();
 
-  toggleSwitch.addEventListener('change', function() {
+  toggleSwitch.addEventListener("change", function () {
     active = !active;
     chrome.storage.local.set({ isActive: active });
     updateUIState();
@@ -208,7 +228,6 @@ document.addEventListener("DOMContentLoaded", function () {
   newListBtn.addEventListener("click", function () {
     window.location.href = "list.html";
   });
- 
 
   async function testt() {
     const result = await new Promise((resolve, reject) => {
@@ -217,11 +236,10 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
     if (result.count) {
-      counterElem.innerHTML = `Word counter: ${result.count}`
+      counterElem.innerHTML = `Word counter: ${result.count}`;
     } else {
       counterElem.innerHTML = `Word counter: 0`;
     }
   }
   testt();
-
 });
