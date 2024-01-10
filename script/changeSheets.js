9502c48c12a87aeba82fbe9843625c011b95590a
Edgar Neverovski <eneverovski@sqad.loc>
2024-01-10 17:19:11 +0200

    Design for guide added

--- script/changeSheets.js
+++ script/changeSheets.js
@@ -1,9 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var button = document.getElementById("getSheets");
-    var input = document.getElementById("textInput");
-    var iframe = document.getElementById("googleSheets");
+    const guide = document.getElementById("guide");
+    const showGuide = document.getElementById("showGuide");
 
+    const button = document.getElementById("getSheets");
+    const input = document.getElementById("textInput");
+    const iframe = document.getElementById("googleSheets");
+    
     button.addEventListener("click", function () {
         iframe.src = input.value + '?widget=true';
-    })
+    });
+
+    showGuide.addEventListener("click", function () {
+        const guideDisplay = window.getComputedStyle(guide).getPropertyValue("display");
+        guide.style.display = (guideDisplay === "none") ? "block" : "none";
+    });
+    
 });
