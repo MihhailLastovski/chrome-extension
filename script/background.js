4489c18cfa582a0d8a53545568e46fc8fae653f4
Mihhail Lastovski <lastovskim@gmail.com>
2024-01-16 17:41:02 +0200

    example

--- script/background.js
+++ script/background.js
@@ -12,4 +12,35 @@ chrome.runtime.onInstalled.addListener(function () {
       chrome.storage.local.set({count: 0});
     }
   });
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === "requestScreenshot") {
+    chrome.tabs.captureVisibleTab(null, { format: "png" }, function (dataUrl) {
+      saveScreenshot(dataUrl);
+    });
+  }
+});
+
+function saveScreenshot(dataUrl) {
+  const blob = dataURItoBlob(dataUrl);
+  const filename = "screenshot.png";
+
+  chrome.downloads.download({
+    url: dataUrl,
+    filename: filename,
+    saveAs: true, 
+  });
+}
+
+function dataURItoBlob(dataURI) {
+  const byteString = atob(dataURI.split(",")[1]);
+  const mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
+  const ab = new ArrayBuffer(byteString.length);
+  const ia = new Uint8Array(ab);
+  for (let i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+  return new Blob([ia], { type: mimeString });
+}
+
+
 });
--- script/contentScript.js
+++ script/contentScript.js
@@ -94,4 +94,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
       });
     }
   }
+  if (request.action === "captureScreenshot") {
+    chrome.runtime.sendMessage({ action: "requestScreenshot" });
+  }
 });
