34b5443b9f377a012b057cca931ebf1a530d1751
Edgar Neverovski <eneverovski@sqad.loc>
2024-02-12 16:28:18 +0200

    Bug fixing. No more counter

--- script/background.js
+++ script/background.js
@@ -43,6 +43,13 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
             });
         });
         return true;
+    } else if (request.action === 'updateBadge') {
+        const count = request.count || 0;
+        chrome.action.setBadgeText({
+            text: count > 0 ? count.toString() : '',
+        });
+        chrome.action.setBadgeBackgroundColor({ color: '#FC0365' });
+        chrome.storage.local.set({ count: count });
     }
 });
 
