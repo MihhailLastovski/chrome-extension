3782d3baf2dd4e9562c2be96f6c6fb18c81d2a57
Mihhail Lastovski <mlastovski@alphacrc.com>
2024-02-15 16:14:54 +0200

    works with publish

--- script/listScript.js
+++ script/listScript.js
@@ -340,19 +340,37 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Чтение слов из CSV файла
     csvListBtn.addEventListener('click', function () {
+        // divWithListImportSettigs.innerHTML = '';
+
+        // var csvInput = document.createElement('input');
+        // csvInput.type = 'text';
+        // csvInput.id = 'textInput';
+        // csvInput.placeholder = 'Paste the link';
+
+        // csvButton.addEventListener('click', function () {
+        //     if (csvInput.value.trim() !== '') {
+        //         csvLink = csvInput.value.replace('/edit', '/export?format=csv');
+        //         chrome.storage.local.set({ dataURL: csvLink });
+        //         fetchDataAndProcessWords(csvLink, true);
+
+        //         csvInput.value = '';
+        //     } else {
+        //         alert('Please enter link');
+        //     }
+        // });
         divWithListImportSettigs.innerHTML = '';
 
         var csvInput = document.createElement('input');
         csvInput.type = 'text';
         csvInput.id = 'textInput';
         csvInput.placeholder = 'Paste the link';
-
+    
         csvButton.addEventListener('click', function () {
             if (csvInput.value.trim() !== '') {
-                csvLink = csvInput.value.replace('/edit', '/export?format=csv');
-                chrome.storage.local.set({ dataURL: csvLink });
-                fetchDataAndProcessWords(csvLink, true);
-
+                const htmlLink = csvInput.value;
+                chrome.storage.local.set({ dataURL: htmlLink });
+                fetchDataAndProcessWords(htmlLink, true);
+    
                 csvInput.value = '';
             } else {
                 alert('Please enter link');
@@ -474,47 +492,68 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
+    // async function fetchDataAndProcessWords(url, readOnly) {
+    //     if (readOnly) {
+    //         try {
+    //             const response = await fetch(url);
+    //             const csvData = await response.text();
+
+    //             const rows = csvData.split('\n');
+    //             const wordsArray = rows.reduce((words, row) => {
+    //                 const columns = row.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+    //                 const wordsInRow = columns.map((cell) =>
+    //                     cell.trim().replace(/"/g, '')
+    //                 );
+    //                 return words.concat(
+    //                     wordsInRow.filter((word) => word !== '')
+    //                 );
+    //             }, []);
+
+    //             wordsArray.forEach((word) => {
+    //                 addWord(word.trim());
+    //             });
+    //         } catch (error) {
+    //             console.error('Error while retrieving data:', error);
+    //             alert('Error while retrieving data, please try again.');
+    //         }
+    //     } else {
+    //         try {
+    //             const response = await fetch(url);
+    //             const csvData = await response.json();
+
+    //             const wordsArray = csvData.map((rowData) => rowData.col1);
+
+    //             wordsArray.forEach((word) => {
+    //                 addWord(word.trim());
+    //             });
+    //         } catch (error) {
+    //             console.error('Ошибка при получении данных:', error);
+    //             alert('Ошибка при получении данных');
+    //         }
+    //     }
+    // }
     async function fetchDataAndProcessWords(url, readOnly) {
-        if (readOnly) {
-            try {
-                const response = await fetch(url);
-                const csvData = await response.text();
+        try {
+            const response = await fetch(url);
+            const htmlData = await response.text();
 
-                const rows = csvData.split('\n');
-                const wordsArray = rows.reduce((words, row) => {
-                    const columns = row.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
-                    const wordsInRow = columns.map((cell) =>
-                        cell.trim().replace(/"/g, '')
-                    );
-                    return words.concat(
-                        wordsInRow.filter((word) => word !== '')
-                    );
-                }, []);
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(htmlData, 'text/html');
+            const tableRows = doc.querySelectorAll('.waffle tbody tr');
 
-                wordsArray.forEach((word) => {
-                    addWord(word.trim());
-                });
-            } catch (error) {
-                console.error('Error while retrieving data:', error);
-                alert('Error while retrieving data, please try again.');
-            }
-        } else {
-            try {
-                const response = await fetch(url);
-                const csvData = await response.json();
+            const wordsArray = Array.from(tableRows).map(row => {
+                const columns = row.querySelectorAll('td');
+                return Array.from(columns).map(column => column.textContent.trim());
+            });
 
-                const wordsArray = csvData.map((rowData) => rowData.col1);
-
-                wordsArray.forEach((word) => {
-                    addWord(word.trim());
-                });
-            } catch (error) {
-                console.error('Ошибка при получении данных:', error);
-                alert('Ошибка при получении данных');
-            }
+            wordsArray.forEach((word) => {
+                addWord(word.join(','));
+            });
+        } catch (error) {
+            console.error('Error while retrieving data:', error);
+            alert('Error while retrieving data, please try again.');
         }
     }
-
     function sendDataToGoogleAppsScript(url, data) {
         fetch(url, {
             method: 'POST',
