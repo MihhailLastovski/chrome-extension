94c56b4f84e31e8fa2581d53a992d0d1ed8ceadd
Edgar Neverovski <eneverovski@sqad.loc>
2024-03-07 17:06:50 +0200

    Extension highlights elements with expected values in attributes

--- script/contentScripts/contentScript.js
+++ script/contentScripts/contentScript.js
@@ -52,112 +52,165 @@ async function getFromLocalStorage(key) {
     });
 }
 
-async function highlightText(searchText, highlightColor, listId = null) {
+// async function highlightText(searchText, highlightColor, listId = null) {
+//     highlightColorRestore = highlightColor;
+
+//     function findWordInWordLists(word) {
+//         for (const wordList of wordLists) {
+//             if (wordList.words && wordList.id === listId) {
+//                 const foundWord = wordList.words.find(
+//                     (wordObj) =>
+//                         wordObj.word.trim().toLowerCase() === word.toLowerCase()
+//                 );
+//                 if (foundWord) {
+//                     return foundWord;
+//                 }
+//             }
+//         }
+//         return null;
+//     }
+
+//     if (boolActive && searchText !== '') {
+//         const searchRegex = new RegExp(searchText, 'gi');
+
+//         function highlightTextNode(node) {
+//             if (
+//                 node.nodeType === Node.TEXT_NODE &&
+//                 !isDescendantOfStyleOrScript(node)
+//             ) {
+//                 const text = node.nodeValue;
+//                 if (searchRegex.test(text)) {
+//                     const foundWord = findWordInWordLists(searchText);
+//                     const isValid =
+//                         foundWord && statusesList.includes(foundWord.status);
+//                     const colorStyle = isValid
+//                         ? `background-color: ${highlightColor}; border: 4px solid ${highlightColor};`
+//                         : `border: 4px solid ${highlightColor};`;
+
+//                     if (node.parentNode.className !== 'highlighted') {
+//                         const wrapper = document.createElement('span');
+//                         wrapper.className = 'highlightedP';
+//                         wrapper.setAttribute('data-list-id', listId);
+
+//                         let lastIndex = 0;
+//                         let match;
+
+//                         searchRegex.lastIndex = 0;
+//                         while ((match = searchRegex.exec(text)) !== null) {
+//                             const beforeMatch = text.substring(
+//                                 lastIndex,
+//                                 match.index
+//                             );
+
+//                             wrapper.appendChild(
+//                                 document.createTextNode(beforeMatch)
+//                             );
+
+//                             const matchedText = document.createElement('span');
+//                             matchedText.className = 'highlighted';
+//                             matchedText.style.cssText = colorStyle;
+//                             matchedText.textContent = match[0];
+//                             if (listId) {
+//                                 matchedText.dataset.listId = listId;
+//                             }
+//                             wrapper.appendChild(matchedText);
+//                             lastIndex = match.index + match[0].length;
+//                         }
+
+//                         wrapper.appendChild(
+//                             document.createTextNode(text.substring(lastIndex))
+//                         );
+
+//                         node.parentNode.replaceChild(wrapper, node);
+//                     }
+//                 }
+//             } else if (
+//                 node.nodeType === Node.ELEMENT_NODE &&
+//                 !['style', 'script'].includes(node.tagName.toLowerCase()) &&
+//                 node.childNodes &&
+//                 node.childNodes.length > 0
+//             ) {
+//                 node.childNodes.forEach((childNode) =>
+//                     highlightTextNode(childNode)
+//                 );
+//             }
+//         }
+
+//         // Проверка, является ли узел потомком элемента style или script
+//         function isDescendantOfStyleOrScript(node) {
+//             while (node.parentNode) {
+//                 node = node.parentNode;
+//                 if (
+//                     node.tagName &&
+//                     (node.tagName.toLowerCase() === 'style' ||
+//                         node.tagName.toLowerCase() === 'script')
+//                 ) {
+//                     return true;
+//                 }
+//             }
+//             return false;
+//         }
+
+//         highlightTextNode(document.body);
+//     }
+//     // Отображение счётчика
+//     chrome.runtime.sendMessage({
+//         action: 'updateBadge',
+//         count: document.querySelectorAll('span.highlighted').length,
+//     });
+// }
+
+async function highlightAttributes(searchText, highlightColor, listId = null) {
     highlightColorRestore = highlightColor;
 
-    function findWordInWordLists(word) {
-        for (const wordList of wordLists) {
-            if (wordList.words && wordList.id === listId) {
-                const foundWord = wordList.words.find(
-                    (wordObj) =>
-                        wordObj.word.trim().toLowerCase() === word.toLowerCase()
-                );
-                if (foundWord) {
-                    return foundWord;
-                }
+    function findAttributeMatch(element) {
+        const attributes = element.attributes;
+        for (const attr of attributes) {
+            if (attr.value.trim().toLowerCase() === searchText.toLowerCase()) {
+                return element;
             }
         }
         return null;
     }
 
     if (boolActive && searchText !== '') {
-        const searchRegex = new RegExp(searchText, 'gi');
+        function highlightElement(element) {
+            const matchedElement = findAttributeMatch(element);
+            if (matchedElement) {
+                if (matchedElement.parentNode.className !== 'highlighted') {
+                    const colorStyle = `border: 4px solid ${highlightColor};`;
 
-        function highlightTextNode(node) {
-            if (
-                node.nodeType === Node.TEXT_NODE &&
-                !isDescendantOfStyleOrScript(node)
-            ) {
-                const text = node.nodeValue;
-                if (searchRegex.test(text)) {
-                    const foundWord = findWordInWordLists(searchText);
-                    const isValid =
-                        foundWord && statusesList.includes(foundWord.status);
-                    const colorStyle = isValid
-                        ? `background-color: ${highlightColor}; border: 4px solid ${highlightColor};`
-                        : `border: 4px solid ${highlightColor};`;
+                    const wrapper = document.createElement('span');
+                    wrapper.className = 'highlighted';
+                    wrapper.style.cssText = colorStyle;
+                    wrapper.setAttribute('data-list-id', listId);
 
-                    if (node.parentNode.className !== 'highlighted') {
-                        const wrapper = document.createElement('span');
-                        wrapper.className = 'highlightedP';
-                        wrapper.setAttribute('data-list-id', listId);
+                    const textNode = document.createTextNode(
+                        element.textContent
+                    );
+                    wrapper.appendChild(textNode);
 
-                        let lastIndex = 0;
-                        let match;
-
-                        searchRegex.lastIndex = 0;
-                        while ((match = searchRegex.exec(text)) !== null) {
-                            const beforeMatch = text.substring(
-                                lastIndex,
-                                match.index
-                            );
-
-                            wrapper.appendChild(
-                                document.createTextNode(beforeMatch)
-                            );
-
-                            const matchedText = document.createElement('span');
-                            matchedText.className = 'highlighted';
-                            matchedText.style.cssText = colorStyle;
-                            matchedText.textContent = match[0];
-                            if (listId) {
-                                matchedText.dataset.listId = listId;
-                            }
-                            wrapper.appendChild(matchedText);
-                            lastIndex = match.index + match[0].length;
-                        }
-
-                        wrapper.appendChild(
-                            document.createTextNode(text.substring(lastIndex))
-                        );
-
-                        node.parentNode.replaceChild(wrapper, node);
-                    }
+                    element.innerHTML = '';
+                    element.appendChild(wrapper);
+                    // element.parentNode.replaceChild(wrapper, element);
                 }
-            } else if (
-                node.nodeType === Node.ELEMENT_NODE &&
-                !['style', 'script'].includes(node.tagName.toLowerCase()) &&
-                node.childNodes &&
-                node.childNodes.length > 0
-            ) {
-                node.childNodes.forEach((childNode) =>
-                    highlightTextNode(childNode)
-                );
             }
         }
 
-        // Проверка, является ли узел потомком элемента style или script
-        function isDescendantOfStyleOrScript(node) {
-            while (node.parentNode) {
-                node = node.parentNode;
-                if (
-                    node.tagName &&
-                    (node.tagName.toLowerCase() === 'style' ||
-                        node.tagName.toLowerCase() === 'script')
-                ) {
-                    return true;
-                }
-            }
-            return false;
-        }
+        const elements = document.querySelectorAll('*');
+        elements.forEach((element) => highlightElement(element));
 
-        highlightTextNode(document.body);
+        // Update badge only if using listId
+        if (listId) {
+            const count = document.querySelectorAll(
+                `span.highlightedP[data-list-id="${listId}"]`
+            ).length;
+            chrome.runtime.sendMessage({
+                action: 'updateBadge',
+                count: count,
+            });
+        }
     }
-    // Отображение счётчика
-    chrome.runtime.sendMessage({
-        action: 'updateBadge',
-        count: document.querySelectorAll('span.highlighted').length,
-    });
 }
 
 chrome.runtime.onMessage.addListener(async function (
@@ -167,7 +220,12 @@ chrome.runtime.onMessage.addListener(async function (
 ) {
     if (request.action === 'highlight') {
         try {
-            await highlightText(
+            // await highlightText(
+            //     request.searchText,
+            //     request.highlightColor,
+            //     request.listId
+            // );
+            await highlightAttributes(
                 request.searchText,
                 request.highlightColor,
                 request.listId
